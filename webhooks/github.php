91dc7b22279bb029277742f4a599a723efb5c08c
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-04-15 11:55:42 +0200

    Update github.php

--- webhooks/github.php
+++ webhooks/github.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]==="POST") {
-    $entityBody = stream_get_contents(STDIN);
+    $entityBody = file_get_contents('php://input');
     echo shell_exec("./github.sh; python3 github.py \"$entityBody\"");
 } else {
     echo $_SERVER['REQUEST_METHOD'];
