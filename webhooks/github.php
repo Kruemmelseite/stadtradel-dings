8ab7daefd20c8febf2a269ca7ffda52e2b9dbcab
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-04-15 11:40:58 +0200

    Update github.php

--- webhooks/github.php
+++ webhooks/github.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]==="POST") {
-    echo shell_exec("./github.sh");
+    echo shell_exec("./github.sh; python3 github.py");
 } else {
     echo $_SERVER['REQUEST_METHOD'];
 }
