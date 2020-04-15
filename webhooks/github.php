8cf89425c9f9029af25d48244503bb8aff65106a
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-04-15 12:37:01 +0200

    Deleted github.py and call

--- webhooks/github.php
+++ webhooks/github.php
@@ -1,9 +1,6 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]==="POST") {
-    $entityBody = file_get_contents('php://input');
-    echo shell_exec("./github.sh;");
-    echo shell_exec("python3 github.py \"$entityBody \"");
-    echo "python3 github.py \"$entityBody \"";
+    echo shell_exec("./github.sh");
 } else {
     echo $_SERVER['REQUEST_METHOD'];
 }
--- webhooks/github.py
+++
@@ -1,5 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('body')
-args = parser.parse_args()
-print(args.body)
