c2a2f4803b2956d42a3fdfbba8cb89f3038339f2
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-05-11 08:52:47 +0200

    Update github.php
    Don't look at Request method

--- webhooks/github.php
+++ webhooks/github.php
@@ -1,7 +1,7 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]==="POST") {
+//if($_SERVER["REQUEST_METHOD"]==="POST") {
     echo shell_exec("./github.sh");
-} else {
-    echo $_SERVER['REQUEST_METHOD'];
-}
+//} else {
+    //echo $_SERVER['REQUEST_METHOD'];
+//}
 ?>
