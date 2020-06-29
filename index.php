b6ea7659778302eeb42f03be882c17c82edd55a8
Squar3Locket <67316583+Squar3Locket@users.noreply.github.com>
2020-06-29 16:45:29 +0200

    Changed to new style
    entries.php
    index.php
    style.css

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <head>
         <meta name="google-site-verification" content="RapvCHY2cGnvXUFpTALBVs2e-r6FyaOxGrglcjLnOdU" />
         <link rel="stylesheet" type="text/css" href="style.css">
-        <title>Der Döner-Blog</title>
+        <title>Würzburg Stadtradeln</title>
     </head>
     <body>
         <div id="login"><?php include("loginform.php")?></div>
-        <div id="title">Der Döner-Blog</div>
+        <div id="title">Würzburg Stadtradeln</div>
         <div id="content"><?php include("entries.php")?></div>
     </body>
 </html>
