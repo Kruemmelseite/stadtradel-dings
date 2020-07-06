c79b174245fa7be05b2511b533b7e50864013588
Kruemmelspalter <54735589+Kruemmelspalter@users.noreply.github.com>
2020-07-06 10:54:47 +0200

    Updated to grid layout

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ if ($_SERVER['REQUEST_URI'] == "/index.php") {
         <link rel="stylesheet" type="text/css" href="style.css">
         <title>Stadtradeln Würzburg</title>
     </head>
-    <body>
-        <div id="login"><?php include "loginform.php"?></div>
+    <body class="container">
         <div id="title">Stadtradeln Würzburg</div>
+        <div id="login"><?php include "loginform.php"?></div>
         <div id="content"><?php include "entries.php"?></div>
     </body>
 </html>
