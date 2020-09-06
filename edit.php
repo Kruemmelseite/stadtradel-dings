bce7f1a2ea3f72c890e88633acc9baa474644e61
Kruemmelspalter <mika.thomas@freenet.de>
2020-09-06 20:41:24 +0200

    Added Publish checkbox

--- edit.php
+++ edit.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['perms']) and ($_SESSION['perms'] & 1 and mysqli_fetch_assoc
         $title = mysqli_real_escape_string($db, $_POST['title']);
         $content = mysqli_real_escape_string($db, $_POST['content']);
         $id = mysqli_real_escape_string($db, $_GET['id']);
-        $published = $_SESSION['perms'] & 8 ? true : false;
+        $published = (($_SESSION['perms'] & 8 ? true : false) && isset($_POST["publish"]) ? 1 : 0) ? 1 : 0;
         mysqli_query($db, "UPDATE entries SET title='$title', content='$content', author=$_SESSION[id], published=$published WHERE id=$id;"); // TODO add column published
         header("Location: .");
     } else if (isset($_GET['id'])) {
@@ -16,12 +16,15 @@ if (isset($_SESSION['perms']) and ($_SESSION['perms'] & 1 and mysqli_fetch_assoc
         $entry = mysqli_fetch_assoc($entries);
         $title = $entry['title'];
         $content = $entry['content'];
+        $checked = $entry["published"] ? "checked=\"checked\"" : "";
         echo <<<FORM
         <div id="content">
             <form action="edit.php?id=$_GET[id]" method="post">
             <input type="text" name="title" placeholder="Überschrift" value='$title'></input><a href=".">Home</a><br>
             <textarea id="editor" cols="65" rows="15" name="content">$content</textarea><br>
-                <input type="submit" value="Posten" id="submit">
+            <label for="publish">Publish</label>
+            <input type="checkbox" id="publish" name="publish" value="true" $checked>
+            <input type="submit" value="Posten" id="submit">
             </form>
         </div>
         <script src="../ckeditor/ckeditor.js"></script>
--- new_entry.php
+++ new_entry.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['perms']) and $_SESSION['perms'] & 1) {
     if (!empty($_POST['title']) and !empty($_POST['content'])) {
         $title = mysqli_real_escape_string($db, $_POST['title']);
         $content = mysqli_real_escape_string($db, $_POST['content']);
-        $published = $_SESSION['perms'] & 8 ? true : false;
+        $published = (($_SESSION['perms'] & 8 ? true : false) && isset($_POST["publish"]) ? 1 : 0) ? 1 : 0;
         mysqli_query($db, "INSERT INTO entries (title, content, author, published) VALUES ('$title','$content','$_SESSION[id]', $published);");
         header("Location: .");
     } else {
@@ -14,6 +14,8 @@ if (isset($_SESSION['perms']) and $_SESSION['perms'] & 1) {
 <form id="content" action="new_entry.php" method="post">
 <input type="text" name="title" placeholder="Überschrift"> <a href="index.php">Home</a><br>⁴
 <textarea id="editor" cols="65" rows="15" name="content"></textarea><br>
+<label for="publish">Publish</label>
+<input type="checkbox" id="publish" name="publish" value="true" checked="checked">
 <input type="submit" value="Posten" id="submit">
 </form>
 
